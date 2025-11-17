41667d9fccdf1addfeed847dc22c6770ac629153
TAYMAZ328 <taymazpc@gmail.com>
2025-11-18 03:12:55 +0330

    update: enhance /add and /del commands with better validation; improve error handling

--- bot/attack.py
+++ bot/attack.py
@@ -2,9 +2,9 @@ from telethon import events
 from random import randint, uniform
 import asyncio
 
-from script import client, get_id
 from util import load_sens, authorize, log_error
 from config import boss_id, user1
+from script import client, get_id
 
 
 
@@ -15,12 +15,16 @@ async def handler(event):
     sender = int(event.sender_id)
     if sender in user1.boss or sender not in user1.enemy: return
     user1.last_msg[sender] = event # store last msg of the enemy for evil mode use
-
-    if not user1.evil and eval("event.is_reply if user1.replied else True"):
-        async with client.action(event.chat_id, 'typing'):
-            delay = user1.delay
-            if delay != 'off': await asyncio.sleep(uniform(delay[0],delay[1]))
-            await event.reply(choose())
+    
+    try:
+        if not user1.evil and eval("event.is_reply if user1.replied else True"):
+            async with client.action(event.chat_id, 'typing'):
+                delay = user1.delay
+                if delay != 'off': await asyncio.sleep(uniform(delay[0],delay[1]))
+                await event.reply(choose())
+    except Exception as e:
+        log_error(f"Error replying to enemy: {event.text}\n{e}")
+        await event.respond("Failed to reply")
 
 
 @client.on(events.NewMessage(pattern=r"/add\s*(?:(.+))?"))
@@ -28,19 +32,17 @@ async def add_enemy(event):
     if not authorize(event): return
 
     user = await get_id(event, d=True)
-    id = int(user.id)
     try:
-        if id in user1.enemy:
-            raise ValueError
-        if not id or id in user1.boss:
-            raise Exception
+        if not user: return await event.reply("User ID couldn't be retrieved")
+        id = int(user.id)
+        if id in user1.enemy: return await event.reply("User is already added")
+        if id in user1.boss: return await event.reply("User is admin")
 
         user1.enemy.append(id)
-        await event.respond(f"Enemy [{id}] added.")
-    except ValueError:
-        await event.respond(f"User is already added")
-    except Exception:
-        await event.respond(f"Couldn't add user.")
+        await event.respond(f"Enemy [[{id}](tg://user?id={id})] added.")
+    except Exception as e:
+        log_error(f"Error adding enemy: {event.text}\n{e}")
+        await event.reply(f"Couldn't add user")
 
 
 @client.on(events.NewMessage(pattern=r"/del\s*(?:(.+))?"))
@@ -48,19 +50,18 @@ async def del_enemy(event):
     if not authorize(event): return
        
     user = await get_id(event, d=True)
-    id = int(user.id)
     try:
-        if id not in user1.enemy:
-            raise ValueError
+        if not user: return await event.reply("User ID couldn't be retrieved")
+        id = int(user.id)
+        if id not in user1.enemy: return await event.reply("User does not exist in Enemy list")
 
         user1.enemy.remove(id)
-        del user1.last_msg[id]
-        await event.respond(f"Enemy [{id}] removed.")
-    except ValueError:
-        await event.respond(f"User does not exist in the Enemy List")
-    except Exception:
-        await event.respond(f"Couldn't delete user.")
+        if id in user1.last_msg: del user1.last_msg[id]
+        await event.respond(f"Enemy [[{id}](tg://user?id={id})] added.")
 
+    except Exception as e:
+        log_error(f"Error deleting enemy: {event.text}\n{e}")
+        await event.reply(f"Couldn't delete user.")
 
 
 @client.on(events.NewMessage(pattern='/enemies'))
@@ -84,24 +85,29 @@ async def force_reply(event):
 async def set_time(event):
     if not authorize(event): return
 
-    delay = event.pattern_match.group(1).split('-')
-    if len(delay) == 2:
-        user1.delay = list(map(int, delay))
-        await event.reply(f"Delay set to {delay[0]}-{delay[1]}")
-        return
-    else:
-        user1.delay = "off"
-        await event.reply(f"Delay Deactivated")
+    try:
+        delay = event.pattern_match.group(1).split('-')
+        if len(delay) == 2:
+            user1.delay = list(map(int, delay))
+            await event.reply(f"Delay set to {delay[0]}-{delay[1]}")
+            return
+        else:
+            user1.delay = "off"
+            await event.reply(f"Delay Deactivated")
+
+    except Exception as e:
+        log_error(f"Error settime: {event.text}\n{e}")
+        await event.reply("Failed to set time")
 
 
 @client.on(events.NewMessage(pattern=r"/setlevel\s*(off|1|2|3)?"))
 async def senlevel(event):
     if not authorize(event): return
 
-    lvl = event.pattern_match.group(1)
-    if not lvl: return await event.reply(f"Level is set to [ {user1.lvl[2]} ]")
-
     try:
+        lvl = event.pattern_match.group(1)
+        if not lvl: return await event.reply(f"Level is set to [ {user1.lvl[2]} ]")
+        
         if lvl == "off":
             user1.lvl = [None, None, "off"]
         elif lvl == "1":
@@ -115,8 +121,8 @@ async def senlevel(event):
         await event.reply(f"Level set to {lvl}")
 
     except Exception as e:
+        log_error(f"Error setlevel: {event.text}\n{e}")
         await event.reply("Failed to set level")
-        log_error(f"{event.text}\n{e}")
 
 
 @client.on(events.NewMessage(pattern=r"/spam (\d+)"))
@@ -134,8 +140,8 @@ async def spaming(event):
                     asyncio.sleep(uniform(delay[0],delay[1]))
                 await event.respond(txt)
     except Exception as e:
-        await event.reply("Spam Failed.")
-        log_error(f"{event.text}\n{e}")
+        log_error(f"Error spamming: {event.text}\n{e}")
+        await event.reply("Spaming Failed")
 
 
 @client.on(events.NewMessage(pattern="/evilmode"))
@@ -149,22 +155,28 @@ async def evil_mode(event):
     user1.evil = True
     await event.respond("Evil mode Activated")
 
-    async with client.action(event.chat_id, 'typing'):
-        while user1.evil and user1.enemy:
-            for enemy in user1.enemy:
-                msg = user1.last_msg.get(enemy, None)
-                if not user1.last_msg:
-                    await event.respond("Last message couldn't be retrieved")
-                    if user1.evil: await evil_mode(event)
-                    return
-                if not msg: break
-                delay = user1.delay
-                if delay != 'off': await asyncio.sleep(uniform(delay[0],delay[1]))
-                await msg.reply(choose())
-        else:
-            if not user1.enemy: await event.reply("No enemy")
+    try:
+        async with client.action(event.chat_id, 'typing'):
+            while user1.evil and user1.enemy:
+                for enemy in user1.enemy:
+                    msg = user1.last_msg.get(enemy, None)
+                    if not user1.last_msg:
+                        await event.respond("Last message couldn't be retrieved")
+                        if user1.evil: await evil_mode(event)
+                        return
+                    if not msg: break
+                    delay = user1.delay
+                    if delay != 'off': await asyncio.sleep(uniform(delay[0],delay[1]))
+                    await msg.reply(choose())
+            else:
+                if not user1.enemy: await event.reply("No enemy")
 
-    if user1.evil: await evil_mode(event)
+        if user1.evil: await evil_mode(event)
+
+    except Exception as e:
+        log_error(f"Evil mode operation failure: {event.text}\n{e}")
+        await event.reply("Evil mode Failed")
+        user1.evil = False
 
 
 @client.on(events.NewMessage(pattern=r"/dcmd\s*(@?\w+)?"))
@@ -172,15 +184,20 @@ async def dcmd(event):
     if not authorize(event): return
 
     user = await get_id(event, d=True)
-    if not user: return
 
-    id = int(user.id)
-    if int(event.sender_id) != boss_id: return await event.reply("Permission denied.")
-    if id == boss_id: return await event.reply("Commander can't be dismissed")
-    if id not in user1.boss: return await event.reply("User is not a commander")
+    try:
+        if not user: return
+        id = int(user.id)
+        if int(event.sender_id) != boss_id: return await event.reply("Permission denied")
+        if id == boss_id: return await event.reply("Commander can't be dismissed")
+        if id not in user1.boss: return await event.reply("User is not a commander")
 
-    user1.boss.remove(id)
-    await event.respond(f"Commander Deleted:\nName: {f"[{user.first_name}](tg://user?id={user.id})"}\nID: {user.id}")
+        user1.boss.remove(id)
+        await event.respond(f"Commander Deleted:\nName: {f"[{user.first_name}](tg://user?id={user.id})"}\nID: {user.id}")
+
+    except Exception as e:
+        log_error(f"Error deleting commander: {event.text}\n{e}")
+        await event.reply("Couldn't delete commander")
 
 
 @client.on(events.NewMessage(pattern=r"/cmds\s*(@?\w+)?"))
@@ -194,15 +211,21 @@ async def acmd(event):
     if not authorize(event): return
 
     user = await get_id(event, d=True)
-    if not user: return
 
-    id = int(user.id)
-    if int(event.sender_id) != boss_id: return await event.reply("Permission denied")
-    if id in user1.enemy: return await event.reply("User is a enemy")
-    if id in user1.boss: return await event.reply("User is already a commnader")
+    try:
+        if not user: return
+        id = int(user.id)
+        if int(event.sender_id) != boss_id: return await event.reply("Permission denied")
+        if id in user1.enemy: return await event.reply("User is a enemy")
+        if id in user1.boss: return await event.reply("User is already a commnader")
 
-    user1.boss.append(id)
-    await event.respond(f"New Commander added:\nName: {f"[{user.first_name}](tg://user?id={user.id})"}\nID: {user.id}")
+        user1.boss.append(id)
+        await event.respond(f"New Commander added:\nName: {f"[{user.first_name}](tg://user?id={user.id})"}\nID: {user.id}")
+
+    except Exception as e:
+        log_error(f"Error adding commander: {event.text}\n{e}")
+        await event.reply("Couldn't add commander")
+
 
 # Randomly choose a sentence
 def choose():
